5400ba8d40bea6612533f06db9327aaf21b88385
Scholar <scholar@paradigm.local>
2023-08-24 13:26:42 -0600

    my first commit

---
+++ intro.py
@@ -0,0 +1,15 @@
+greeting = input("Hello, possible pirate! What's the password?")
+
+if greeting in ("Arrr!"):
+
+	print("Go away, pirate.")
+
+elif greeting:
+    print("Greetings, hater of pirates!")
+    
+if greeting in ("ice face"):
+    print("🥶👌")
+
+if greeting in ("chuck"):
+    print("A mouse by the name of Charles Entertainment Cheese")
+    print("Welcome Lord Cheese")
