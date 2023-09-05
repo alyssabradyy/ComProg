24c76b218b41a8fac41ff0c950854a8c451ac86b
Scholar <scholar@paradigm.local>
2023-09-05 12:36:58 -0600

    created variables and a function

---
+++ variablestest.py
@@ -0,0 +1,13 @@
+(name) = ("Alyssa")
+(test_score) = 100/100
+(was_lunch_tasty) = "No"
+(bday_classes) = {
+    ("first"): ("seminary"),
+    ("second"): ("digital literacy"),
+    ("third"): ("computer programming"),
+    ("fourth"): ("french")
+}
+
+equation = 4+2
+print("The answer is....!")
+print(equation)
