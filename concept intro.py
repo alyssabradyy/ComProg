1281edbcdf1fda89582c207386aeb84859639f2d
Scholar <scholar@paradigm.local>
2023-09-07 13:30:55 -0600

    intro to concepts

---
+++ concept intro.py
@@ -0,0 +1,38 @@
+from re import X
+
+
+my_name = "Alyssa"
+
+def give_a_name():
+    a_name = "William"
+    print(a_name)
+
+user_input = input("What is your name? ")
+print(f"Nice to meet you, {user_input}!")
+
+# data types
+my_string = "This is my string."
+my_integer = 4
+my_float = 5.67
+is_my_boolean = True
+my_declaration = ""
+my_initialization = "Hello world"
+
+print(my_integer + my_float)
+
+add = 2 + 3
+subtract = 5 - 1
+multiply = 3 * 8
+divide = 6 / 2
+modulo = 10 / 3
+
+
+statement = 4 > 2
+statement2 = 4 >= 2
+statement3 = 4 == 2
+statement4 = 4 != 2
+
+message = 4 > 2 and 33 < 43
+message2 = 4 < 2 or 33 < 43
+x = False
+print(not x)
--- variablestest.py
+++ variablestest.py
@@ -6,9 +6,6 @@ bday_classes = ["seminary", "digital literacy", "computer programming", "french"
 def add_two_numbers(num1, num2):
     print(num1 + num2)
 add_two_numbers(4, 2)
-
-print("The answer is....!")
-
 user_number1 = int(input("Give me a number: "))
 user_number2 = int(input("Give me a second number: "))
 
