90c6b9cc355485315be366710991c30f24a1be21
Scholar <scholar@paradigm.local>
2023-09-19 13:32:02 -0600

    control

--- intro_to_control.py
+++ intro_to_control.py
@@ -1,23 +1,23 @@
-# car_color = "Blue"
-# car_make = "tesla"
+car_color = "Blue"
+car_make = "tesla"
 
-# if car_color == "Blue" or car_color == "blue":
-#     if car_make == "tesla":
-#         print("My car is a blue tesla")
-#     else:
-#         print("My car is blue")
-# elif car_color == "Red":
-#     print('My car is a red ferrari')
-# else:
-#     print("I just have a bike")
+if car_color == "Blue" or car_color == "blue":
+    if car_make == "tesla":
+        print("My car is a blue tesla")
+    else:
+        print("My car is blue")
+elif car_color == "Red":
+    print('My car is a red ferrari')
+else:
+    print("I just have a bike")
 
-#     for loop
-# for i in range(1, 10):
-#     print(i)
+    #for loop
+for i in range(1, 10):
+    print(i)
 
-# sports = ['soccer', 'basketball', 'hockey', 'football']
-# for item in sports:
-#     print(item)
+sports = ['soccer', 'basketball', 'hockey', 'football']
+for item in sports:
+    print(item)
 
 i = 1
 while i < 6:
