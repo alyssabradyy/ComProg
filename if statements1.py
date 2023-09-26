3af4b5c2d877cb8671922fd73693af6dc06a7ab6
Scholar <scholar@paradigm.local>
2023-09-26 13:26:40 -0600

    if statements

---
+++ if statements1.py
@@ -0,0 +1,43 @@
+# from curses.textpad import rectangle
+
+
+# user_salary = int(input("What is your salary? "))
+
+# bonus_requirement = 5
+# years_employed = int(input("How many years have you been employed here? "))
+# salary_bonus = user_salary * 1.05
+
+# if years_employed >= bonus_requirement:
+#     print("You get a 5 percent bonus!")
+#     print(f"Your salary is {salary_bonus}. Good Job!")
+
+# elif years_employed < bonus_requirement:
+#     print("You do not get a bonus yet.")
+#     print(f"Your salary is {user_salary}.")
+
+
+# rectangle_length = int(input("What is the length of the rectangle? "))
+# rectangle_width = int(input("What is the width of the rectangle? "))
+
+# if rectangle_length == rectangle_width :
+#     print("This shape is a square")
+# else:
+#     print("This shape is not a square")
+
+
+# num1 = int(input("Give me a number "))
+# num2 = int(input("Give me another number "))
+
+# if num1 > num2:
+#     print(f"The number {num1} is greater than {num2}.")
+# elif num1 < num2:
+#     print(f"The number {num2} is greater than {num1}.")
+
+
+age1 = int(input("I need the ages of 3 people. What is the first person's age? "))
+age2 = int(input("What is the age of the second person? "))
+age3 = int(input("How old is the last person? "))
+
+oldest = ()
+middle = ()
+youngest = () 
