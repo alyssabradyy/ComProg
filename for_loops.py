550f67049aea6b1cba52f7c32e30b9d9f790727f
Scholar <scholar@paradigm.local>
2023-09-28 13:29:41 -0600

    forloops

---
+++ for_loops.py
@@ -0,0 +1,28 @@
+school = ('p', 'a', 'r', 'a', 'd', 'i', 'g', 'm')
+for i in school:
+    print(i)
+
+for x in range(5):
+    print(x)
+
+animals = ('cat', 'dog', 'tiger', 'cow')
+for n in animals:
+    print(n)
+
+flowers = ('Jasmine', 'Lotus', 'Rose', 'Sunflower')
+for f in flowers:
+    print(f)
+else:
+    print("Done")
+
+list1 = (5, 10, 15, 20)
+list2 = ('Tomatoes', 'Potatoes', 'Carrots', 'Cucumbers')
+for z in list1:
+    for y in list2:
+        print(z, y)
+
+vehicles = ('Car', 'Cycle', 'Bus', 'Tempo')
+for v in vehicles:
+    print(v)
+    if v == 'Bus':
+        break
