58f3d435a4685f0bf2be3530693abb2e559bf8a2
Scholar <scholar@paradigm.local>
2023-10-05 13:31:36 -0600

    sub days

---
+++ fibonacci.py
@@ -0,0 +1,6 @@
+num_entered = int(input("How many digits of Fibonacci would you like? "))
+fibonacci = 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
+for x in num_entered:
+    x = fibonacci
+    if num_entered == fibonacci:
+        break
---
+++ rockpaperclamp.py
@@ -0,0 +1,3 @@
+print("Welcome to Rock, Paper, Clamp! This is a 2 player game, so I hope you brought a friend!")
+player1 = input("Would Player 1 like to play rock, paper, or clamp? ")
+player2 = input("Would Player 2 like to play rock, pape, or clamp? ")
