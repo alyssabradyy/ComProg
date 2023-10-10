f25a4e6b4b093533859c9b6262ca9f26957498e8
Scholar <scholar@paradigm.local>
2023-10-10 13:28:53 -0600

    challenges

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,12 @@
-num_entered = int(input("How many digits of Fibonacci would you like? "))
-fibonacci = 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
-for x in num_entered:
-    x = fibonacci
-    if num_entered == fibonacci:
-        break
+from re import X
+
+
+how_deep = int(input("How deep into the Fibonacci would you like to go? "))
+def fibonacci(num):
+    for i in range(num):
+        x = 1
+        y = x
+        z = x + y
+        x = z
+        print(x)
+fibonacci(how_deep)
--- for_loops.py
+++ for_loops.py
@@ -1,51 +1,68 @@
-# school = ('p', 'a', 'r', 'a', 'd', 'i', 'g', 'm')
-# for i in school:
-#     print(i)
+school = ('p', 'a', 'r', 'a', 'd', 'i', 'g', 'm')
+for i in school:
+    print(i)
 
-# for x in range(5):
-#     print(x)
+for x in range(5):
+    print(x)
 
-# animals = ('cat', 'dog', 'tiger', 'cow')
-# for n in animals:
-#     print(n)
+animals = ('cat', 'dog', 'tiger', 'cow')
+for n in animals:
+    print(n)
 
-# flowers = ('Jasmine', 'Lotus', 'Rose', 'Sunflower')
-# for f in flowers:
-#     print(f)
-# else:
-#     print("Done")
+flowers = ('Jasmine', 'Lotus', 'Rose', 'Sunflower')
+for f in flowers:
+    print(f)
+else:
+    print("Done")
 
-# list1 = (5, 10, 15, 20)
-# list2 = ('Tomatoes', 'Potatoes', 'Carrots', 'Cucumbers')
-# for z in list1:
-#     for y in list2:
-#         print(z, y)
+list1 = (5, 10, 15, 20)
+list2 = ('Tomatoes', 'Potatoes', 'Carrots', 'Cucumbers')
+for z in list1:
+    for y in list2:
+        print(z, y)
 
-# vehicles = ('Car', 'Cycle', 'Bus', 'Tempo')
-# for v in vehicles:
-#     print(v)
-#     if v == 'Bus':
-#         break
+vehicles = ('Car', 'Cycle', 'Bus', 'Tempo')
+for v in vehicles:
+    print(v)
+    if v == 'Bus':
+        break
 
-# for m in vehicles:
-#     if m == 'Bus':
-#         continue
-#     print(m)
+for m in vehicles:
+    if m == 'Bus':
+        continue
+    print(m)
 
-# numbers = (12, 3, 56, 67, 89, 90)
-# sum = 0
+numbers = (12, 3, 56, 67, 89, 90)
+sum = 0
 
-# for i in numbers:
-#     sum += i
-# print(sum)
+for i in numbers:
+    sum += i
+print(sum)
 
-# for q in numbers:
-#     enumerate = q
-# print(enumerate)
+for q in numbers:
+    enumerate = q
+print(enumerate)
 
-# for i in range(1, 101):
-#     if(i % 5 == 0):
-#         print(i)
+for i in range(1, 101):
+    if(i % 5 == 0):
+        print(i)
 
-nums = (3, 6, 8, 14, 27, 87, 54, 39, 21, 86, 23, 76, 90, 2345, 567, 89,345, 56, 679, 780, 345, 253, 68)
-max(nums)
+fruit_list = ['Mango', 'Banana', 'Orange']
+new_list = []
+for r in fruit_list:
+    new_list.append(r)
+print(new_list)
+
+lst = [34, 7, 12, 22, 5, 87, 57, 3]
+largest = lst[0]
+for i in lst:
+    if i > largest:
+        largest = i
+print(largest)
+
+minimum = lst[0]
+for i in lst:
+    if minimum > i:
+        minimum = i
+print(minimum)
+
