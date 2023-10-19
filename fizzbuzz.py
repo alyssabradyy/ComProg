ade0020b57623ca918307c911c84c58f3dc92d0c
Scholar <scholar@paradigm.local>
2023-10-19 13:26:16 -0600

    fizz buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,19 @@
+user_number = int(input("What number between 1 and 12 would you like to replace? "))
+message = input("What word would you like to replace this number with? ")
+
 for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print("FizzBuzz")
+    elif i % user_number == 0:
+        print(message)
     elif i % 3 == 0:
         print("Fizz")
     elif i % 5 == 0:
         print("Buzz")
     else:
         print(i)
+    for i in range(2, i):
+        if (i % i) == 0:
+            break
+        else:
+            print("Prime")
