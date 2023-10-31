7190e3931688a5d7c40ccf4f63c5f66ed25616ec
Scholar <scholar@paradigm.local>
2023-10-31 13:31:38 -0600

    interview

---
+++ interview.py
@@ -0,0 +1,8 @@
+iname = input("Hello, thank you for coming! I am Alyssa Brady, the hiring manager here. Will you please remind me of your name? ")
+print(f"Nice to meet you {iname}!")
+foi = input("What types of programming are you interested in? ")
+experience = input("How many years of experience do you have? ")
+salary = input("What would be your desired salary? ")
+position = input("What is your desired position in the company? ")
+reference = input("How did you find out about this job opening? ")
+deez
