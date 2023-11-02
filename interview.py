24b2d2454b7651e0343d388298d443fb45ac5512
Scholar <scholar@paradigm.local>
2023-11-02 13:31:57 -0600

    interview

--- interview.py
+++ interview.py
@@ -1,8 +1,23 @@
 iname = input("Hello, thank you for coming! I am Alyssa Brady, the hiring manager here. Will you please remind me of your name? ")
-print(f"Nice to meet you {iname}!")
 foi = input("What types of programming are you interested in? ")
-experience = input("How many years of experience do you have? ")
-salary = input("What would be your desired salary? ")
+if foi in ("python") or foi in ("Python"):
+    print("Python is the main program we use so that is excellent.")
+else:
+    print("We mainly use Python here so it may take a while for you to learn it but its still an option.")
+experience = input(int("How many years of experience do you have? "))
+if experience >= 3:
+    print("That's wonderful!")
+elif experience < 3:
+    print("We usually look for more experience but I'm sure we could work with it.")
+salary = input(int("What would be your desired salary? "))
+if salary <= 6000:
+    print("If that's a good number for you, we can definitely work with it.")
+elif salary > 6000:
+    print("That's a bit higher than our starting salary but I'm sure we could negotiate if needed.")
 position = input("What is your desired position in the company? ")
 reference = input("How did you find out about this job opening? ")
-deez
+motivation = input("What about this job appeals to you? ")
+strengths = input("What are some of your greatest strengths? ")
+weaknesses = input("What are some of your greatest weaknesses? ")
+print(f"So to recap, you are {iname}, the programming you are interested in is {foi}, you have {experience} years of experience, your desired salary is {salary}, your desired position is {position}, you found this job through {reference}, you applied to this job because of {motivation}, your strengths are {strengths} and your weaknesses are {weaknesses}.")
+ending = input("Do you have any questions of your own before we end? ")
