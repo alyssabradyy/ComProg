6fb7235f6eabeb542eaea3e505dbc699104a1fbe
Scholar <scholar@paradigm.local>
2023-11-07 12:39:01 -0700

    interview return

--- interview.py
+++ interview.py
@@ -4,16 +4,19 @@ if foi in ("python") or foi in ("Python"):
     print("Python is the main program we use so that is excellent.")
 else:
     print("We mainly use Python here so it may take a while for you to learn it but its still an option.")
-experience = input(int("How many years of experience do you have? "))
+
+experience = int(input("How many years of experience do you have? "))
 if experience >= 3:
     print("That's wonderful!")
 elif experience < 3:
     print("We usually look for more experience but I'm sure we could work with it.")
-salary = input(int("What would be your desired salary? "))
+
+salary = int(input("What would be your desired salary? "))
 if salary <= 6000:
     print("If that's a good number for you, we can definitely work with it.")
 elif salary > 6000:
     print("That's a bit higher than our starting salary but I'm sure we could negotiate if needed.")
+
 position = input("What is your desired position in the company? ")
 reference = input("How did you find out about this job opening? ")
 motivation = input("What about this job appeals to you? ")
