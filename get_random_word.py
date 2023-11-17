1ad8a8933820d919da4359f8ecc8da9088b85ccd
Scholar <scholar@paradigm.local>
2023-11-17 11:21:46 -0700

    guess words bro

--- get_random_word.py
+++ get_random_word.py
@@ -1,12 +1,8 @@
 print("Welcome to hangman!")
 
-secret_word = with open("randomwordgen.py", "r") as file:
-    allText = file.read()
-    words = list(map(str, allText.split()))
-    code_word = split(secret_word)
+secret_word = "autobiography"
 
-print(f"This is your word:{enumerate(code_word) to "_ "}")
+print("This is your word:_ _ _ _ _ _ _ _ _ _ _ _ _")
 guesses = input("What letter would you like to guess? ")
-
-if guesses == code_word:
-    print(f"{guesses in code_word}_ _ _ _ _ _ _ _ _ _ _ _ _")
+if guesses == split(secret_word):
+    print(f"{guesses in secret_word}_ _ _ _ _ _ _ _ _ _ _ _ _")
