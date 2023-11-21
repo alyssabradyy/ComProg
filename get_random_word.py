b8d551cbf8e4c908caf08716286d6e8964bc01a3
Scholar <scholar@paradigm.local>
2023-11-21 12:59:02 -0700

    hangmans

--- get_random_word.py
+++ get_random_word.py
@@ -1,8 +1,22 @@
-print("Welcome to hangman!")
-
-secret_word = "autobiography"
-
-print("This is your word:_ _ _ _ _ _ _ _ _ _ _ _ _")
-guesses = input("What letter would you like to guess? ")
-if guesses == split(secret_word):
-    print(f"{guesses in secret_word}_ _ _ _ _ _ _ _ _ _ _ _ _")
+word = "awesome"
+guessed = "_" *len(word)
+guessed = list(guessed)
+lstGuessed = []
+letter = input("guess letter: ")
+while True:
+    if letter in lstGuessed:
+        letter = ''
+        print("Already guessed!")
+    elif letter in word:
+        index = word.index(letter)
+        guessed[index] = letter
+        word[index] = '_'
+    else:
+        print(''.join(guessed))
+        if letter != '':
+            lstGuessed.append(letter)
+        letter = input("guess letter: ")
+    
+    if '_' not in guessed:
+        print("You won!")
+        break
