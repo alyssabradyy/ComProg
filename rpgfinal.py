d4f2b6e777058a0245c97771411a951fe2cfe6d2
Scholar <scholar@paradigm.local>
2023-11-30 13:32:41 -0700

    final project

---
+++ rpgfinal.py
@@ -0,0 +1,8 @@
+charname = input("Welcome to the Battle Nexus! Here you can become a great hero, what is your title? ")
+print(f"Pleased to meet you {charname}! You will start with 100 HP and nothing in your inventory. Try to win as many fights as you can and collect different prizes.")
+print("To check your items press the 'r' key, to walk forward, press 'w', and if you encounter an enemy you may press either 'a' for attack, or 'f' for flee.")
+walk = input("Go ahead and press 'w' to walk!")
+if walk in ("w"):
+    while True:
+        import random
+        with open (1, 2, 3):
