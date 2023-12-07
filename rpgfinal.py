38cfa51f8f9745a4237434f49d2c591fa76733eb
Scholar <scholar@paradigm.local>
2023-12-07 13:35:31 -0700

    final

--- rpgfinal.py
+++ rpgfinal.py
@@ -2,6 +2,8 @@ charname = input("Welcome to the Battle Nexus! Here you can become a great hero,
 print(f"Pleased to meet you {charname}! You will start with 100 HP and nothing in your inventory. Try to win as many fights as you can and collect different prizes.")
 print("To check your items press the 'r' key, to walk forward, press 'w', and if you encounter an enemy you may press either 'a' for attack, or 'f' for flee.")
 enemies = ('tiger', 'cyclops', 'vampire', 'unicorn', 'samurai')
+char_hp = 100
+enemy_hp = 100
 walk = input("Go ahead and press 'w' to walk!")
 if walk in ("w"):
     while True:
@@ -10,3 +12,9 @@ if walk in ("w"):
             if 1:
                 enemy_encounter = input(f"You found an opponent! This enemy is a {random(enemies)}. Will you attack or flee? ")
                 if enemy_encounter in "a":
+                    while char_hp > 0 or while enemy_hp > 0:
+                        attack_given = random(range(1, 100))
+                        enemy_hp - attack_given
+                elif enemy_encounter in "f":
+                else:
+                    ("Please press 'a' to attack or 'f' to flee.")
