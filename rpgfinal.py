f1a98bf2dac531420069ae9f68a317c28bb53708
Scholar <scholar@paradigm.local>
2023-12-08 11:26:18 -0700

    rpg final

--- rpgfinal.py
+++ rpgfinal.py
@@ -1,7 +1,7 @@
 charname = input("Welcome to the Battle Nexus! Here you can become a great hero, what is your title? ")
 print(f"Pleased to meet you {charname}! You will start with 100 HP and nothing in your inventory. Try to win as many fights as you can and collect different prizes.")
 print("To check your items press the 'r' key, to walk forward, press 'w', and if you encounter an enemy you may press either 'a' for attack, or 'f' for flee.")
-enemies = ('tiger', 'cyclops', 'vampire', 'unicorn', 'samurai')
+enemies = ('alien', 'cyclops', 'vampire', 'unicorn', 'samurai')
 char_hp = 100
 enemy_hp = 100
 walk = input("Go ahead and press 'w' to walk!")
@@ -12,7 +12,7 @@ if walk in ("w"):
             if 1:
                 enemy_encounter = input(f"You found an opponent! This enemy is a {random(enemies)}. Will you attack or flee? ")
                 if enemy_encounter in "a":
-                    while char_hp > 0 or while enemy_hp > 0:
+                    while char_hp > 0 or enemy_hp > 0:
                         attack_given = random(range(1, 100))
                         enemy_hp - attack_given
                         attack_taken = random(range(1, 100))
@@ -23,14 +23,14 @@ if walk in ("w"):
                         print("You ran from the fight!")
                     elif run_chance == 2:
                         print("You cannot run from this battle.")
-                        while char_hp > 0 or while enemy_hp > 0:
+                        while char_hp > 0 or enemy_hp > 0:
                             attack_given = random(range(1, 100))
                             enemy_hp - attack_given
                             attack_taken = random(range(1, 100))
                             char_hp - attack_taken                    
                 else:
                     ("Please press 'a' to attack or 'f' to flee.")
-            if char_hp = 0:
+            if char_hp == 0:
                 print("You died! Better luck next time!")
-            elif enemy_hp = 0:
-                print("You won this round!")
+            elif enemy_hp == 0:
+                print("You won this round! You got treasure! Open your inventory to check!")
